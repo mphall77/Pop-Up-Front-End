92057041d5d69d1b6345caaa99e97a7a9814d86b
Marsha Hall <mphall77@gmail.com>
2021-07-25 15:04:36 -0400

    edit image constraints

--- front-end/src/Components/ProductListItem.js
+++ front-end/src/Components/ProductListItem.js
@@ -4,13 +4,15 @@ const ProductListItem = ({ product }) => {
 	return (
 		<div class="col">
 			<div class="card">
-				<Link to={`/products/${product.id}`} target="_blank" rel="noreferrer">
-					<img
-						src={product.photo}
-						class="card-img-top product-img"
-						alt="card-group"
-					/>
-				</Link>
+				<div className="card-parent">
+					<Link to={`/products/${product.id}`} target="_blank" rel="noreferrer">
+						<img
+							src={product.photo}
+							class="card-img-top product-img"
+							alt="card-group"
+						/>
+					</Link>
+				</div>
 				<div class="card-body product-desc">
 					<h5 class="card-title">{product.name}</h5>
 					<p class="card-text">{product.description} </p>
