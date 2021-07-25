c96575c6529678e7762efd930b8634d4a2e3f1d4
Marsha Hall <mphall77@gmail.com>
2021-07-25 10:44:42 -0400

    update css for list

--- front-end/src/Components/ProductListItem.js
+++ front-end/src/Components/ProductListItem.js
@@ -1,8 +1,26 @@
 import { Link } from "react-router-dom";
 
 const ProductListItem = ({ product }) => {
-  return (
-    <tr>
+	return (
+		<div class="col">
+			<div class="card">
+				<img src={product.photo} class="card-img-top" alt="card-group-image" />
+				<div class="card-body">
+					<h5 class="card-title">{product.name}</h5>
+					<p class="card-text">{product.description} </p>
+				</div>
+				<div class="card-footer">
+					<small class="text-muted"> {product.price}</small>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProductListItem;
+
+{
+	/* <tr>
       <td>
         <Link to={`/products/${product.id}`} target="_blank" rel="noreferrer">
           {product.name}
@@ -13,8 +31,5 @@ const ProductListItem = ({ product }) => {
       </td>
       <td>{product.description}</td>
       <td>${product.price}</td>
-    </tr>
-  );
-};
-
-export default ProductListItem;
+    </tr> */
+}
