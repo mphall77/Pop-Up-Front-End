00584750d24f48e88ef3f573bd257607b33ccc8d
Marsha Hall <mphall77@gmail.com>
2021-07-25 15:29:53 -0400

    add home button

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    return(
-        <div>
-            <h1>Welcome to the Pop-Up Shop!</h1>
-        </div>
-    )
+	return (
+		<div>
+			<h1>Welcome to the Pop-Up Shop!</h1>
+			<Link to="/products">
+				<button type="button" class="btn btn-primary">
+					Enter the Shop
+				</button>
+			</Link>
+		</div>
+	);
 }
