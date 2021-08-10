fd5f3cf19ca3e78e409d3686281729579c491693
Marsha Hall <mphall77@gmail.com>
2021-08-09 20:31:11 -0400

    edit readme

--- front-end/src/Components/ProductListItem.js
+++ front-end/src/Components/ProductListItem.js
@@ -4,21 +4,24 @@ const ProductListItem = ({ product }) => {
 	return (
 		<div className="col">
 			<div className="card">
-				<div className="card-parent">
-					<Link to={`/products/${product.id}`} rel="noreferrer">
-						<img
-							src={product.photo}
-							className="card-img-top product-img"
-							alt="card-group"
-						/>
-					</Link>
-				</div>
-				<div className="card-body product-desc">
-					<h5 className="card-title">{product.name}</h5>
-					<p className="card-text">{product.description} </p>
-				</div>
-				<div className="card-footer">
-					<small className="text-muted"> ${product.price}</small>
+				<div className="d-flex flex-column align-items-center mt-3 embed-responsive embed-responsive-16by9">
+					<div className="card-parent">
+						<Link to={`/products/${product.id}`} rel="noreferrer">
+							<img
+								src={product.photo}
+								className="card-img-top product-img"
+								alt="card-group"
+							/>
+						</Link>
+					</div>
+
+					<div className="card-body product-desc">
+						<h5 className="card-title">{product.name}</h5>
+						<p className="card-text">{product.description} </p>
+					</div>
+					<div className="card-footer">
+						<small className="text-muted"> ${product.price}</small>
+					</div>
 				</div>
 			</div>
 		</div>
